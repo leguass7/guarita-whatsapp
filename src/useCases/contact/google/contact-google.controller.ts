a0ff82c83f3f47ce2656ada7910d0d96116d1026
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2022-04-30 15:36:59 -0300

    docs: save page

--- src/useCases/contact/google/contact-google.controller.ts
+++ src/useCases/contact/google/contact-google.controller.ts
@@ -5,12 +5,6 @@ import type { GoogleService } from '#/services/google.service';
 export class ContactGoogleController {
   constructor(private googleService: GoogleService) {}
 
-  async code(req: Request, res: Response, _next: NextFunction): Promise<any> {
-    const { query } = req;
-    const code = query?.code as string;
-    return res.status(200).send({ success: true, code }).end();
-  }
-
   async google(req: Request, res: Response, _next: NextFunction): Promise<any> {
     const contacts = await this.googleService.getContacts();
 
