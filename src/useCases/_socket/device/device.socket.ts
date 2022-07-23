d5b63bb9de7cd920502992800a40ab07e08dcfce
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2022-07-23 15:00:35 -0300

    test: teste de conexão socket

--- src/useCases/_socket/device/device.socket.ts
+++ src/useCases/_socket/device/device.socket.ts
@@ -4,6 +4,7 @@ const SocketDeviceRoute = SocketRouter();
 
 SocketDeviceRoute.use('device/count', async (sockerServerService, socket, req, res) => {
   console.log('req.phone', req.phone);
+  console.log('req.waId', req.waId);
   res({ success: true, message: 'ok' });
 });
 
