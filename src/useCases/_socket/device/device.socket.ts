08eb1dc6a0ede18be0c64db3bf74773c8971e372
severoemanuel <fs.severoemanuel@gmail.com>
2022-08-18 13:17:40 -0300

    feat: added query with params from request in socket device and report routes

--- src/useCases/_socket/device/device.socket.ts
+++ src/useCases/_socket/device/device.socket.ts
@@ -1,4 +1,5 @@
 import { api } from '#/services/Api';
+import { logDev } from '#/services/LoggerService';
 import { SocketRouter } from '#/services/SocketServerService';
 
 const SocketDeviceRoute = SocketRouter();
@@ -6,8 +7,10 @@ const SocketDeviceRoute = SocketRouter();
 const path = 'device/count';
 
 SocketDeviceRoute.use(path, async (sockerServerService, socket, req, res) => {
-  const response = await api.getDefault(path);
-  console.log('resposta do servidor guarita: ', response);
+  const query = new URLSearchParams({ ...req }).toString();
+  const response = await api.getDefault(`${path}?${query}`);
+
+  logDev('resposta do servidor guarita: ', JSON.stringify(response));
 
   res({ success: true, message: 'estamos processando sua solicitação, por favor aguarde.' });
 });
--- src/useCases/_socket/report/wakeup-alarm.socket.ts
+++ src/useCases/_socket/report/wakeup-alarm.socket.ts
@@ -1,4 +1,5 @@
 import { api } from '#/services/Api';
+import { logDev } from '#/services/LoggerService';
 import { SocketRouter } from '#/services/SocketServerService';
 
 const SocketReportWakeupRoute = SocketRouter();
@@ -6,8 +7,10 @@ const SocketReportWakeupRoute = SocketRouter();
 const path = 'report/wakeup-alarm';
 
 SocketReportWakeupRoute.use(path, async (sockerServerService, socket, req, res) => {
-  const response = await api.getDefault(path);
-  console.log('resposta do servidor guarita: ', response);
+  const query = new URLSearchParams({ ...req }).toString();
+  const response = await api.getDefault(`${path}?${query}`);
+
+  logDev('resposta do servidor guarita: ', JSON.stringify(response));
 
   res({ success: true, message: 'estamos processando sua solicitação, por favor aguarde.' });
 });
