99968c359a87fd2084727a3dbbe2bc1deab5f22e
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2023-02-18 14:00:50 -0300

    chore(release): 1.0.13

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,5 +2,5 @@
  * NÃO MODIFICAR: Arquivo criado por script automatizado
  * - './scripts/release.js'
  */
-export const appVersion = '1.0.12';
+export const appVersion = '1.0.13';
 export const appName = 'guarita-whatsapp';
