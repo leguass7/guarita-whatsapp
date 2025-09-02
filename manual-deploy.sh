d5027742771d22e0f51f2b95144577441de360ed
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2025-09-01 23:13:27 -0300

    ci: testes

--- manual-deploy.sh
+++ manual-deploy.sh
@@ -2,7 +2,7 @@
 docker-compose -f "docker-compose.yml" down --volumes
 
 git pull
-
-DOCKER_BUILDKIT=1 docker-compose -f "docker-compose.yml" up -d --build
+export DOCKER_BUILDKIT=1
+docker-compose -f "docker-compose.yml" up -d --build
 
 docker logs -f guarita-whatsapp
